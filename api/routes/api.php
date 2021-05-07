7fb59a72d5ee825a085ed22e5feee953c34c8cd0
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-07 13:48:58 -0300

    Removing not used route

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 
-Route::get('auth/login', [AuthController::class, 'login'])->name('login');
-Route::post('auth/login', [AuthController::class, 'login'])->name('login');
+Route::post('auth/login', [AuthController::class, 'login'])->name('auth.login');
 
 Route::group(['middleware' => ['jwt-auth'] ], function(){
 
