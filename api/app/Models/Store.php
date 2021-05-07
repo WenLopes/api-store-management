88bd17d7ee47847ef45545140425272e4aa17191
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-07 16:42:59 -0300

    Creating Store model

---
+++ api/app/Models/Store.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Store extends Model
+{
+    use HasFactory;
+    
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $table = 'store';
+
+    /**
+     * Primary key name
+     *
+     * @var string
+     */
+    protected $primaryKey = 'store_id';
+
+    /**
+     * @var string[]
+     */
+    protected $guarded = [];
+}
